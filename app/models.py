f4867f182be91d8960580e042793058204995c62
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-09 14:09:24 +0300

    added a class method

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 from sqlalchemy.sql import func
-# from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class Pitch(db.Model):
@@ -20,6 +20,22 @@ class User(db.Model):
     password = db.Column(db.String (225))
     first_name = db.Column(db.String (225))
     pitches = db.relationship('Pitch')
-
-    # def __repr__(self):
+    pass_secure = db.Column(db.String(255))
+     # def __repr__(self):
     #     return f'User {self.first_name}'
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+
+   
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Pitch
-# from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
@@ -9,8 +9,8 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 # Creating app instance
 
