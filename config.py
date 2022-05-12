d268f02c0b49aa27e57cbc2444c06265fa1fea75
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-12 15:44:11 +0300

    initial commit to heroku

--- config.py
+++ config.py
@@ -3,21 +3,14 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # UPLOADED_PHOTOS_DEST ='app/static/photos' 
     
-    #email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
 
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
 
 class TestConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch_test'
+    
     pass
 
 class DevConfig(Config):
--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
+app = create_app('production')
+app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('server',Server)
@@ -12,11 +14,18 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-# Creating app instance
+@manager.command
+def test():
+    '''
+    Run the unit tests
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db,User = User, Pitch = Pitch)
+    return dict(app = app, db = db,User = User, Pitch = Pitch,Comment=Comment)
 
 if __name__ == '__main__':
     manager.run()
