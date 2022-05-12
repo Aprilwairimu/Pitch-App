ebbaf7c18bf138180811f0d0c8706d4c2ee67dda
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-05-12 18:04:00 +0300

    second commit

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 import os
 class Config:    
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
 
 class TestConfig(Config):
     
     pass
 
 class DevConfig(Config):
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://april:2222@localhost/pitch'
     DEBUG = True
 
 config_options = {
--- manage.py
+++ manage.py
@@ -4,9 +4,8 @@ from app.models import User,Pitch
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
 app = create_app('production')
-app = create_app('test')
+
 
 manager = Manager(app)
 manager.add_command('server',Server)
